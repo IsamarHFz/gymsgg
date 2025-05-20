b464ee7bacb9399a7e3457268b8f800f9865a4d9
Xenon <alguirre2@gmail.com>
2025-05-20 13:30:47 -0600

    reques de home

---
+++ src/components/home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+    <p>pulll regues del home</p>
+    <h1 className='border border-red'>Este es home todo bonito todo buapo</h1>
+    </>
+  )
+}
